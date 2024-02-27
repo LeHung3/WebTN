60d667faae4acd3e8ceb87a57b545147a6c25df4
quan <nnmhqn2003@gmail.com>
2024-02-27 11:41:49 +0700

    signUp-signIn

---
+++ login.js
@@ -0,0 +1,21 @@
+
+function checkLogin (username,password, registeredUsers) {
+    for (var i = 0; i < registeredUsers.length; i++) {
+        if(username === registeredUsers[i].username && password === registeredUsers[i].password){
+            alert("Login successful!");
+        } else {
+            alert("Invalid username or password. Please try again.");
+        }
+    }
+}
+
+document.getElementById("loginForm").addEventListener("submit", function(event){
+    event.preventDefault(); // Prevent default form submission
+    var username = document.getElementById("loginUsername").value;
+    var password = document.getElementById("loginPassword").value;
+    var arrayString  = localStorage.getItem('data');
+    var registeredUsers = JSON.parse(arrayString);
+    checkLogin (username,password,registeredUsers)
+    // Validate username and password (demo)
+});
+
---
+++ register.js
@@ -0,0 +1,45 @@
+var registeredUsers = [];
+function registerUser (username, email, password) {
+    var newUser = {
+        username: username,
+        email: email,
+        password: password
+    };
+    registeredUsers.push(newUser);
+}
+function validateRegistration (username, email) {
+    // Kiểm tra xem username hoặc email đã được sử dụng chưa
+    for (var i = 0; i < registeredUsers.length; i++) {
+        if (registeredUsers[i].username === username) {
+            return false; // Username đã tồn tại
+        }
+        if (registeredUsers[i].email === email) {
+            return false; // Email đã được sử dụng
+        }
+    }
+    return true; // Thông tin đăng ký hợp lệ
+}
+document.getElementById("registerForm").addEventListener("submit", function(event){
+    //event.preventDefault(); // Prevent default form submission
+    var username = document.getElementById("registerUsername").value;
+    var email = document.getElementById("registerEmail").value;
+    var password = document.getElementById("registerPassword").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    console.log(1)
+    // Validate registration info (demo)
+    if(password !== confirmPassword){
+      alert("Passwords do not match. Please try again.");
+      return;
+    }
+    else if(!validateRegistration(username,password)){
+        alert("Account already exists.");
+      return;
+    }
+    // Perform registration (demo)
+    else{
+        alert("Registration successful!");
+        registerUser(username,email,password)
+        var arrayString = JSON.stringify(registeredUsers);
+        localStorage.setItem('data', arrayString);
+    }
+});
