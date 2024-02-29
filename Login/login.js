846db7222785bcbde785f9e1451f6662e883e838
Vuong Nguyen <108895030+VuongNguyen333@users.noreply.github.com>
2024-03-01 04:23:57 +0700

    Update login.js

--- Login/login.js
+++ Login/login.js
@@ -1,5 +1,6 @@
 var registeredUsers = JSON.parse(localStorage.getItem('data'));
 function checkLogin (username,password) {
+    if (registeredUsers===null || registeredUsers.length===0) alert("Do not have an account? Please register")
     for (var i = 0; i < registeredUsers.length; i++) {
         if(username === registeredUsers[i].username && password === registeredUsers[i].password){
             return true
@@ -12,5 +13,5 @@ document.getElementById("loginForm").addEventListener("submit", function(event){
     event.preventDefault(); // Prevent default form submission
     var username = document.getElementById("loginUsername").value;
     var password = document.getElementById("loginPassword").value;
-    checkLogin(username,password) ? window.location.href = "../Home/home.html":alert("Invalid username or password. Please try again.")
+    checkLogin(username,password) ? window.location.href="../Home/home.html" :alert("Invalid username or password. Please try again.")
 });
