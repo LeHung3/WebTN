a8fe0bde3089e8e4ff1145395d37f0a905516ccb
Vuong Nguyen <108895030+VuongNguyen333@users.noreply.github.com>
2024-03-01 04:24:23 +0700

    Update register.js

--- Register/register.js
+++ Register/register.js
@@ -1,13 +1,20 @@
-var registeredUsers=JSON.parse(localStorage.getItem("data"));
-function registerUser (username, email, password) {
+
+async function registerUser(username, email, password) {
+    var registeredUsers = JSON.parse(localStorage.getItem("data")) || [];
     var newUser = {
         username: username,
         email: email,
         password: password
     };
-    registeredUsers.push(newUser);
+    console.log('🚀 ~ registerUser ~ newUser:', newUser)
+    await registeredUsers.push(newUser);
+    var jsonRegisteredUsers = JSON.stringify(registeredUsers);
+    console.log('🚀 ~ registerUser ~ jsonRegisteredUsers:', jsonRegisteredUsers)
+    localStorage.setItem('data', jsonRegisteredUsers);
 }
-function validateRegistration (username, email) {
+function validateRegistration(username, email) {
+    var registeredUsers = JSON.parse(localStorage.getItem("data")) || [];
+    if (registeredUsers===null || registeredUsers.length===0) return true
     // Kiểm tra xem username hoặc email đã được sử dụng chưa
     for (var i = 0; i < registeredUsers.length; i++) {
         if (registeredUsers[i].username === username) {
@@ -19,24 +26,24 @@ function validateRegistration (username, email) {
     }
     return true; // Thông tin đăng ký hợp lệ
 }
-document.getElementById("registerForm").addEventListener("submit", function(event){
+document.getElementById("registerForm").addEventListener("submit", async function (event) {
     event.preventDefault()
+    var registeredUsers = JSON.parse(localStorage.getItem("data")) || [];
     var username = document.getElementById("registerUsername").value;
     var email = document.getElementById("registerEmail").value;
     var password = document.getElementById("registerPassword").value;
     var confirmPassword = document.getElementById("confirmPassword").value;
-    if(password !== confirmPassword){
-      alert("Passwords do not match. Please try again.");
-      return;
+    if (!username || !email || !password || !confirmPassword) alert("Please fill in completely")
+    else if (password !== confirmPassword) {
+        alert("Passwords do not match. Please try again.");
+        return;
     }
-    else if(!validateRegistration(username,password)){
+    else if (!validateRegistration(username, password)) {
         alert("Account already exists.");
-      return;
+        return;
     }
-    else{
+    else {
         alert("Registration successful!");
-        registerUser(username,email,password)
-        var jsonRegisteredUsers = JSON.stringify(registeredUsers);
-        localStorage.setItem('data', jsonRegisteredUsers);
+        await registerUser(username, email, password)
     }
 });
