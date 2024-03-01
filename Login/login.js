23adb94089928ac0e8015e9b5194c8088423ed6b
vuongNguyenVg <vuongnguyen332003@gmail.com>
2024-03-01 16:39:48 +0700

    statisticPage

--- Login/login.js
+++ Login/login.js
@@ -1,17 +1,18 @@
 var registeredUsers = JSON.parse(localStorage.getItem('data'));
-function checkLogin (username,password) {
-    if (registeredUsers===null || registeredUsers.length===0) alert("Do not have an account? Please register")
+function checkLogin(username, password) {
+    if (registeredUsers === null || registeredUsers.length === 0) alert("Do not have an account? Please register")
     for (var i = 0; i < registeredUsers.length; i++) {
-        if(username === registeredUsers[i].username && password === registeredUsers[i].password){
+        if (username === registeredUsers[i].username && password === registeredUsers[i].password) {
             return true
         }
     }
     return false
 }
 
-document.getElementById("loginForm").addEventListener("submit", function(event){
+document.getElementById("loginForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent default form submission
+    getData()
     var username = document.getElementById("loginUsername").value;
     var password = document.getElementById("loginPassword").value;
-    checkLogin(username,password) ? window.location.href="../Home/home.html" :alert("Invalid username or password. Please try again.")
+    checkLogin(username, password) ? window.location.href = "../Home/home.html" : alert("Invalid username or password. Please try again.")
 });
